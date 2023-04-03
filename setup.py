552f986e0b9d6faf50818f283e380ec12db38227
conornally <conor.nally@hotmail.com>
2023-04-03 11:17:53 +0100

    reverting setup changes

--- setup.py
+++ setup.py
@@ -3,5 +3,4 @@
 from setuptools import setup
 if __name__=="__main__":
     setup(  scripts=["bin/starbug2","bin/starbug2-match"],
-            data_files=[("extras",["extras/starbug2.completion"])],
             long_description_content_type = "text/markdown")
