6aad65a84ed3fc35ed682a14a2a4866761f1f6fb
conornally <conor.nally@hotmail.com>
2023-11-23 12:04:15 +0000

    tests update

--- tests/test_run.py
+++ tests/test_run.py
@@ -17,37 +17,37 @@ def test_param():
     clean()
     assert run("starbug2 --local-param")==EXIT_EARLY
     assert run("starbug2 --update-param")==EXIT_EARLY
-    assert run("starbug2 -p starbug.param dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -p starbug.param tests/dat/image.fits")==EXIT_SUCCESS
     #run("rm starbug.param")
 
 def test_detect():
     clean()
     #wget("https://app.box.com/s/f2trqcln5mjug3rigs9246202ztuu5fh", "image.fits")
-    assert run("starbug2 -v dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -D dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 --detect dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -D -sSIGSKY=3 -sSIGSRC=15 dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -v tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -D tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 --detect tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -D -sSIGSKY=3 -sSIGSRC=15 tests/dat/image.fits")==EXIT_SUCCESS
     #run("rm image*.fits")
 
 def test_bgd():
     clean()
-    assert run("starbug2 -D dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -d dat/image-ap.fits -B dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -d dat/image-ap.fits --background dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -vf -B dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -D tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -d tests/dat/image-ap.fits -B tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -d tests/dat/image-ap.fits --background tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -vf -B tests/dat/image.fits")==EXIT_SUCCESS
 
 def test_psf():
     clean()
-    assert run("starbug2 -DB dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -d dat/image-ap.fits -b dat/image-bgd.fits -P dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -fP dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -fBP dat/image.fits")==EXIT_SUCCESS
-    assert run("starbug2 -fPs GEN_RESIDUAL=1 dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -DB tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -d tests/dat/image-ap.fits -b tests/dat/image-bgd.fits -P tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -fP tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -fBP tests/dat/image.fits")==EXIT_SUCCESS
+    assert run("starbug2 -fPs GEN_RESIDUAL=1 tests/dat/image.fits")==EXIT_SUCCESS
 
 def clean():
-    files=glob.glob("dat/*")
+    files=glob.glob("tests/dat/*")
     print(files)
-    files.remove("dat/image.fits")
+    files.remove("tests/dat/image.fits")
     for fname in files: os.remove(fname)
 
 
