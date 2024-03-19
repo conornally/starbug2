009c24c3fe729ce0f88574f33e0061995eab43bb
conornally <conor.nally@hotmail.com>
2024-03-19 16:09:37 +0000

    rm test

--- tests/xtest_artificialstars.py
+++ tests/xtest_artificialstars.py
@@ -1,10 +1,10 @@
 from starbug2.bin.afs import afs_main
-from starbug2.bin import EXIT_SUCCESS
+from starbug2.bin import EXIT_SUCCESS, EXIT_FAIL
 from starbug2.artificialstars import Artificial_StarsIII
 
 run = lambda s : afs_main(["starbug2-afs"]+s.split())
 
-def test_run():
+def _test_run():
     assert run("tests/dat/image.fits")==EXIT_SUCCESS
     assert run("nope")==EXIT_FAIL
     
